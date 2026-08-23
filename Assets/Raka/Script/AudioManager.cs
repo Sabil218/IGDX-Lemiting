058bbe631fa61c13031ce86546a04bbe77f353ad
Rakhyan21 <rakawishnu5@gmail.com>
2026-08-23 13:12:29 +0700

    Implemen & Pause

--- Assets/Raka/Script/AudioManager.cs
+++ Assets/Raka/Script/AudioManager.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 public class AudioManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
     public Slider musicSlider;
     public Slider sfxSlider;
+
+    public Slider pauseMusicSlider;
+    public Slider pauseSFXSlider;
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
+
+        pauseMusicSlider.value = volume;
 
         if (volume <= 0.0001f)
         {
@@ -25,6 +31,40 @@ public class AudioManager : MonoBehaviour
     public void SetSFXVolume()
     {
         float volume = sfxSlider.value;
+
+        pauseSFXSlider.value = volume;
+
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("SFXVolume", -80f);
+        }
+        else
+        {
+            audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20f);
+        }
+    }
+
+    public void SetPauseMusicVolume()
+    {
+        float volume = pauseMusicSlider.value;
+
+        musicSlider.value = volume;
+
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("MusicVolume", -80f);
+        }
+        else
+        {
+            audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20f);
+        }
+    }
+
+    public void SetPauseSFXVolume()
+    {
+        float volume = pauseSFXSlider.value;
+
+        sfxSlider.value = volume;
 
         if (volume <= 0.0001f)
         {
